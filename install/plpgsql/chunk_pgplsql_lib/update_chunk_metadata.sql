78d900cb896284b1948dc10debb481b133b5df34
Lee Horner <eyupmiduck2005@gmail.com>
2023-04-28 16:36:24 -0700

    fix retry bug

--- install/plpgsql/chunk_pgplsql_lib/update_chunk_metadata.sql
+++ install/plpgsql/chunk_pgplsql_lib/update_chunk_metadata.sql
@@ -11,16 +11,22 @@ CREATE OR REPLACE PROCEDURE chunk_pgplsql_lib.update_chunk_metadata(
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    UPDATE chunk_data.chunk_boundary b
-    SET end_timestamp = NOW(),
-        sub_chunk_number = i_sub_chunk_number
-    WHERE b.chunk_run_id = i_run_id
-    AND   b.chunk_number = i_chunk_number;
 
     IF i_is_final_sub_chunk THEN
         UPDATE chunk_data.chunk_run d
         SET last_completed_chunk = i_chunk_number
         WHERE d.id = i_run_id;
+
+        UPDATE chunk_data.chunk_boundary b
+        SET end_timestamp = NOW(),
+            sub_chunk_number = i_sub_chunk_number
+        WHERE b.chunk_run_id = i_run_id
+        AND   b.chunk_number = i_chunk_number;
+    ELSE
+        UPDATE chunk_data.chunk_boundary b
+        SET sub_chunk_number = i_sub_chunk_number
+        WHERE b.chunk_run_id = i_run_id
+        AND   b.chunk_number = i_chunk_number;
     END IF;
 
     IF i_is_final_chunk THEN
